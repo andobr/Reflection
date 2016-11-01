cce65d89152b62d575effc4fc55a644973d6e8d1
Anton <Anton@ANTON>
2016-11-01 15:23:55 +0500

    задание 1

---
+++ Application/Plugin1/Plugin1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework;
+
+namespace Plugin1
+{
+    public class Plugin1 : IPlugin
+    {
+        public string Name => "Plugin1";
+    }
+}
---
+++ Application/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Framework;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = @"C:\Users\Anton\Source\Repos\Reflection\";
+            var dllPath = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories);
+
+            foreach (var pluginName in GetNamesOfPlugins(dllPath))
+                Console.WriteLine(pluginName);
+
+            Console.Read();         
+        }
+
+        static List<string> GetNamesOfPlugins(string[] path)
+        {
+            var result = new List<string>();
+
+            foreach (var item in path)
+            {
+                result.AddRange(
+                    Assembly.LoadFrom(item)
+                    .GetTypes()
+                    .Select(Activator.CreateInstance)
+                    .Select(x => ((IPlugin)x).Name)
+                    .ToList());
+            }
+                
+            return result;
+        }
+    }
+}
